0368da842ca747d33abe8cd97b40b8dddcb551cf
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-04-11 18:12:56 +0200

    feat: init setup

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { HStack, Stack, Center, useMediaQuery } from '@chakra-ui/react'
+
+export default function BlogLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const [isDesktop] = useMediaQuery('(min-width: 1000px')
+
+  return (
+    <HStack w='full' alignItems='top' spacing='0'>
+      {isDesktop && (
+        <>
+          <Stack w='500px' />
+          <Stack
+            w='500px'
+            h='100vh'
+            position='fixed'
+            left='0'
+            top='0'
+            bgColor='teal.100'
+          />
+        </>
+      )}
+      {!isDesktop && (
+        <>
+          <Stack w='50px' />
+          <Stack
+            w='50px'
+            h='100vh'
+            position='fixed'
+            left='0'
+            top='0'
+            bgColor='teal.100'
+          />
+        </>
+      )}
+      <Center w='full'>
+        <Stack
+          maxW='80ch'
+          textAlign='left'
+          lineHeight='1.62'
+          spacing='8'
+          py={{ base: '8', lg: '32' }}
+          px={{ base: '4', lg: '0' }}
+        >
+          {children}
+        </Stack>
+      </Center>
+    </HStack>
+  )
+}
