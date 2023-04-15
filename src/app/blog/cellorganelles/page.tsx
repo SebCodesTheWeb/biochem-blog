b9ecf57f815d6631af7846e8b5ca6bbbbf3144f7
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-04-15 12:52:22 +0200

    docs(cell-organelles): wrote second article

---
+++ src/app/blog/cellorganelles/page.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import { MarkdownViewer } from '../../../components/MarkdownViewer'
+
+export default function CellOrganelles() {
+  return (
+    <>
+      <MarkdownViewer>
+        {`
+# Cell Organelles
+** Compartments that makes up a subunit of a cell and performs one or many specific tasks **  
+<br />
+
+Cell organelles ensures vital biologial functions such as homeostasis, energy production and cell replication. The animal cell could be described as containing the following:
+![Animal cell with organelles](https://s3-us-west-2.amazonaws.com/courses-images-archive-read-only/wp-content/uploads/sites/18/2014/07/19180924/0312_Animal_Cell_and_Components.jpg)
+Here follows a brief overview of the above organelles for the animal cell:
+
+### Mitochondria
+Mitochondria are cell organelles active in the process of cellular respiration via the generation of ATP. It has a double membrane, where the inner one is deeply folded, creating a structure called cristae. By folding in a larger surface area could be covered in the same volume, making more room for the electron transport chain. The space between the cristae, is called the matrix, where other chemical processes such as the breakdown of fatty acids and the citric acid cycle take place.
+
+<br/>
+##### Endosymbiotic theory
+Mitochondria has multiple interesting properties, a double membrane, circular DNA, ribosomes and own reproduction via binary fussion. 
+Cicular DNA and binary fussion are usually features present in prokaryotic cells, such as bacteria. These are all pieces of evidence that support the endosymbiotic theory, which states that mitochondria most likely derived from an aerobic bacteria(a bacteria able to do cellular respiration) that was engulfed by an eukaryotic cell and later on lived in symbiosis. 
+Through comparison of DNA sequence this specific aerobic bacteria is thought to have been alphaproteobacteria. 
+The double membrane is also evidence that could support this theory, as the outer membrane is thought of as a consenquence of the engulfment process, where the eukaryotic cells own membrane formed to outer membrane, whilst the inner membrane is the original membrane of the bacteria. 
+
+##### Chloroplasts
+Chloroplasts are another organelle which typically occur in plant cells. Like mitochondria, they are believed to have been instantiated by a symbiotic relationship with bacteria. Chloroplasts are active in the process of photosynthesis, where light energy is transformed into glucose.
+
+
+
+### Plasma membrane
+![Cell membrane](https://www.genome.gov/sites/default/files/tg/en/illustration/plasma_membrane.jpg)
+The plasma membrane encapsulates all other cell organelles in a way that controls the passage of what substances both go in and out. The cell membrane mainly consists of two layers of phospholipids. The enviroment inside the cell is water based, as well as the outside of the cell. Phospholipids have the special property of having a water soluble head(the phosphate molecule along with the choline group) and a non water soluble body(the two fatty acids). This can act together to form a selectively permeable membrane. Small nonpolar molecules without charge can easily pass through the cell membrane, whilst bigger polar, or charged molecules need require certain transport proteins to facilitate their passage into the cell. The cell membrane also hosts a number of receptors, used for communicating with other cells
+
+### Microtubule
+Microtubules are long structures of the protein tubulin formed in a rope/tunnel like structure. It upholds the rigidity and structure of cell, plays a vital role in mitosis by forming the mitotic spindle that seperates sister chromosomes, and facilitates intracellular transport of vesicles by use of motor proteins. It is built up by two variants of the protein tubulin, the alpha and beta variant, these are placed adjacent to each other in a row called protofilament, at one end of the protofilament, it will start with alpha tubulin which becomes the negative end, and on the other end there will be beta tubulin which becomes the positive end. These protofilaments are organized into cylinders of about 13 protofilaments in every circle slice. Motor protein Kinesin can bond to the microtubule and with a vesicle, and then pull the vesicle forward along the microtubule towards the positive end by "walking". Dynein can do the same thing to the negative end.
+![Microtubule](https://www.mechanobio.info/wp-content/uploads/2017/06/microtubule-assembly.jpg)
+
+### Centrosome
+Centrosomes are a type of so called MTOCs(MicroTubule Organizing Centers) which take part in the anchoring, organizing and formation of microtubule. They are formed by two centriols(which themselved are a cylindrical formation of 9 tripplets of microtubule) that lie perpendicular to each other, and that are sewn in by pericentriol material (PCM) (peri is greek for surround), which is an highly dense mass of various proteins which surrounds the centriols in a spherical shape. PCM contains gamma tubulin where the negative end of microtubule can anchor and grow. There is usually only one centrosome per cell which lies near the nucleus, during mitosis the centrosome move to opposite ends of the cell, duplicates and positions on opposite ends. It then forms microtubule which pulls the sister chromosomes away from each other. 
+![Centrosome](https://d3jlfsfsyc6yvi.cloudfront.net/image/mw:1024/q:85/https%3A%2F%2Fhaygot.s3.amazonaws.com%3A443%2Fcheatsheet%2F10903.png)
+
+### Micro and intermediate filament
+Micro and intermediate filaments are complementary to microtubules that together a form the cytoskeleton. Microfilaments are about 5-9nm in diameter, and are made of two intertwined polymers of the protein aktin. Except structural purposes, it also plays a key role in cell movement, like crawling and muscle contraction(with the aid of protein myosin). Like microtubules, microfilaments also have a positive and negative end. Intermediate filaments consist of various proteins, like keratin, and take part in structures such as hair and nails. 
+![Microfilaments](https://ohiostate.pressbooks.pub/app/uploads/sites/36/2017/07/Filaments-and-Microtubules-1024x864.jpg)
+
+### Lysosome
+Lysosomes are membrane bound organelle made from the golgia apparatus which contains a variety of enzymes, specialised to break down a broad range of biomolecules. These can act to break down damaged cell organelles or other substances in a process called autophagy. Lysosomes can also help to breka down pathogens. They can also break down the entire cell in apoptosis(programed cell suicide) 
+
+### Endoplasmic reticulum
+The endoplasmic reticulum is a membrane(phospilipid) enclosed volume(lumen) connected to the nuclear envelope. It is seperated into two parts, rough and smooth.
+##### rER
+![rough endoplasmic reticulum](https://thebiologynotes.com/wp-content/uploads/2021/10/Rough-Endoplasmic-Reticulum-RER.jpeg)
+The rough endoplasmic reticulum contains ribosomes at its outside, hence a _rough_ surface. mRNA from the nucleus might arrive to this ribosomes where proteins will by synthesized and go inside the ER lumen and through the rest of the reticulum. These proteins might undergo modification, mainly a process where carbohydrates are attached to the protein, forming glycoproteins. This process is called N-linked glycosylation, where the N stands for nitrogen, as the carbohydrate is connected to the nitrogen atom of the asparagine amino acid. When the protein reaches the end of the rER it will package up into a vesicle and be marked with COP II protein that labels it for transport into the golgia apparatus.
+##### sER
+![Smooth endoplasmic reticulum](https://info.gbiosciences.com/hubfs/ER.png)
+In contrast to rER, sER does not possess ribosomes at its surface. This makes it unusable for protein synthesis. Instead It's mainly active in the synthesis of lipids, through a process where Acetyl CoA is transported into the sER, and after multiple reactions with different enzymes could be turned into either a fatty acid, phosplipid or steroid. 
+<br />
+<br />
+Except for being the key production center of lipids it also helps breaking down harmful drugs, by attaching polar molecule parts such as hydroxyl groups to the molecules, which make them more water soluble and easier for excretion later on.
+<br />
+<br />
+sER also has glucose-6-phosphatase enzyme on its membrane. During the conversion from glycogen to glucose, glucose-6-phosphates might form as a residue product. This molecule cannot exit the body, however the glucose-6-phosphatase can break down the phosphate group and release free glucose, which can then leave the body.
+![muscle cell](https://upload.wikimedia.org/wikipedia/commons/9/94/1023_T-tubule.jpg)
+sER also serves with microfilaments to create muscle contraction. A modified sER(called sarcoendoplasmic reticulum) located in muscle cells uses ATP powered transport proteins called "Calcium ATPase", this transport calcium ions into the sER where they are stored. During deplorisation stimulation this calcium ions are released, which later on bind to microfilaments that contract the muscle cell.
+
+### Vesicles
+![vesicle](https://images.ctfassets.net/vrrt8fsfwf0e/65in5FgUk7aGK156WJrNxH/986b7b570eb904494993b85204f2db4c/Vesicle_Art_1.svg)
+Vesicles are structures enclosed by a bilayer of phospholipids. This creates a tiny membrane that can safely contain substances either within the cell (transport vesicle) or out of it(secretory vesicles) by exocytosis. They are generally formed from the endoplasmic reticulum or from the golgia apparatus, or through endocytosis as endocytic vesicles. They can transport their contents by attaching to a motor protein such a kinesin or dynein and move along microtubules towards their destination.
+<iframe width="560" height="315" src="https://www.youtube.com/embed/y-uuk4Pr2i8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+### Peroxisomes
+Peroxisomes are small membrane enclosed volumes with certain enzymes, oxidases used for beta and alpha oxidation of fatty acids. This can have multiple use cases. By beta oxidation it can break down long fatty acids into shorter ones, that can then be sent into the mitochondria. Whilst doing this reaction, hydrogen peroxide (H<sub>2</sub>O<sub>2</sub>) might form, which is a dangerous radical that can damage proteins, however the peroxisomes also contain the enzyme catalase, which transformes hydrogen peroxide into O<sub>2</sub> and water. Other dangerous peroxides can be sent into the peroxisomes to be neutralised. Peroxisomes can also act similiarly to that of the sER, for example Acetyl CoA is formed as a product after the oxidation, which will be used to synthesize lipids.
+
+### Vacuoles
+![Vacuole](https://biologydictionary.net/wp-content/uploads/2016/12/Plant-cell-structure-vacuole.jpg)
+Vacuoles are a main component of plant cells, altough they may also occur in animal cell in smaller sizes. They are a big enclosed volume of fluid, where nutrients, ions ans waste products could be stored. They provide structure to the plant cell and act as storage/waste compartments. Carbohydrates and amino acids are often stored inside the vacuole. There is also water and ions which help regulate the osmotic pressure of the cell. This vacoule can also take up multiple dangerous waste and isolate them from the rest of the cell, and then get rid of it via exocytosis.
+
+### Cytoplasm
+Cytoplasm is defined as all the contents inside the cell membrane(except the nucleus), that being the organelles and fluid(cytosol) between organelles. Often one refers to the latter. Cytosol mainly consists of water(about 70-90% of volume), dissolved ions such as sodium/potassiam/calcium. It can also contain biomolecules like enzymes and RNA, as well as small molecules, for example glucose, amino acids and nucleotides. The cytosol maintains a good enviroment of the right PH, ion concentration and osmotic pressure for the cells organelles to function, as well as serving as a site for many chemical process, such as glycolysis(the first breakdown of glucose in cellular respiration).
+
+### Golgi Apparatus
+The golgi apparatus is a part of the endomembrane system. It can take in vesicles from the ER, and further modify them. They might trim the sugar residue form N linked glycogenolysis that occured in rER and make it shorter or package it in a vesicle and send it back marked with COP I protein. It might also perform O linked glycogenolysis, in which a carbohydrate is attached to the hydroxyl group(more specifically the oxygen atom) of ethier serine(Ser) or Threonine(Thr). It might also add lipids or phosphate groups. When it is done it will make a vesicle to transport the protein, which may either travel to the cell membrane to become a receptor/channel/antigen protein, be excreted to become an enzyme/hormone, or be kept within the cell, for example lysosomes.
+![Golgi apparatus](https://www.news-medical.net/image.axd?picture=2016%2F6%2FGolgi_body_shutterstock_300259154.jpg)
+
+### Nucleus and protein synthesis
+The nucleus is the part of the cell where the DNA is stored. It is covered by the nuclear envelope, a membrane continous with ER. The membrane is a bilayer of phosphilipids with a layer of lamin protein on the inside, that are active in multiple processs wihtin the nucleus, such as transcription, translation and organizing DNA. Within the nucleus, there is chromatin, which is packaged DNA, this might be read and transcribed into mRNA, tRNA, and rRNA. This RNA will later leave the nucleus via the nuclear pores, which are transport proteins in the nuclear envelope. This can then enter ribosomes. tRNA will connect to amino acids which then match with mRNA to build up to a protein by the ribosomes. The ribosomes themselves are made of rRNA and ribosomal subunits. The ribosomal subunits are made up of protein and rRNA and are themselves produced by a ribosome, they are later transported into the nucleus by the nuclear pores and into the nucleolus, which is a special part of the nucleus where rRNA and ribosomal subunits are put together into ribosomes, which will later be transported out into the free cytosol or rER.
+![Nucleus](https://micro.magnet.fsu.edu/cells/nucleus/images/nucleusfigure1.jpg)
+
+
+
+`}
+      </MarkdownViewer>
+    </>
+  )
+}
+ 
