9fcecc4fc072f8bd20b379ca8ff211a1fd7ef78f
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-04-24 09:03:26 +0200

    docs(catabolism): added page

---
+++ src/app/blog/catabolism/page.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import { MarkdownViewer } from '../../../components/MarkdownViewer'
+
+export default function Catabolism() {
+  return (
+    <>
+      <MarkdownViewer>
+        {`
+# Catabolism
+How the body breaks down complex molecules into simpler components to release energy, which is essential for maintaining and regulating various biological processes and overall homeostasis.
+
+### Basic physiology 
+
+##### Homeostasis
+Homeostasis, is a term used to refer to the bodys ability to maintain a stable environment by multiple processes. This stable values could be things such as pH, body temperature, ion concentration. Processes which help stabilize these are split into two main categories, negative and positive feedback.
+
+<br />
+##### Negative feedback
+Negative feedback is when the body issues a response to a change which directly counter acts by changing in the opposite direction. Say the bodys temperature increased, then a negative feedback would be that of sweat, as this directly decreases the body temperature.
+
+<br />
+
+##### Positive feedback
+Positive feedback is when the body reinforces a change to later on return back to normal. An example of this is in the case of heart muscle cells, that when their membrane potential is risen above its action potential it increases rapidly during depolarization. To later on be set back during repolarization.
+
+<br />
+
+##### Metabolism
+Metabolism is the term used to describe all processes that occurs within the body to maintain itself, which is usually split into two broad sub categories, catabolism and anabolism. Catabolism is the process of breaking down larger molecules into smaller ones. Anabolism on the other hand is the process of putting together smaller molecules to form larger molecules. A basic example of this is how a human might digest a piece of steak, break down the protein within the steak to the individual amino acids, and then use those to build our own proteins for our body. Basal metabolic rate refers to the minimum energy intake the body needs to maintain itself. This can vary widly on factors such as height, weight, age, activity and sex. A common value is around 1500 kcal. Where one kcal is equivalent to the energy needed to heat one kg of water with one degree celsius, which is around 4.18kj.
+
+<br />
+
+##### Body temperature
+Most of animals have two different strategies when it comes to maintaining body heat, these are homeothermic and poikilothermic animals. A homeothermic animal needs to maintain a stable body temperature and therefore needs to digest more food to fuel their internal body processes for heat generation. This means that they are better adapted to  live in a wide range of environments and that they can endure tougher prolonged physical efforts. They do need more energy to function though. A poikilothermic on the other hand relies on external factors such as the sun to heat them up. This means they are better adapted to survive in one specific climate. Due to not needing to maintain their own body temperature they require a lot less food, and can hibernate for months if times were to get tough. 
+
+<br />
+
+##### Tissues
+These body processes are handled by the different organ systems, which are built up of organs. All organs are built up off different tissues, which are a way to categorise specialised cells with similiar origin. This are regarded as epithelial tissue, which covers surfaces, handles absorption, filtering and secretion of different substances; examples includes the organ which is our skin. Connective tissue provides support and protection, examples includes fat, bone, cartilage, blood. Muslce tissue is all tissue needed for body movement, such as skeletal muscles, the cardiac muscle and smooth muscles. Finally, nerve tissue is all tissue present in the transfer of information, such as nerves.
+
+### Digestive system overview
+![Digestive system overview human](https://www.niddk.nih.gov/-/media/Images/Health-Information/Digestive-Diseases/The_Digestive_System_450x531.jpg)
+1. After food has been been put into the mouth, the food is mechanically broken down by teeth. Salivary glands excrete saliva which contains enzymes like amylase which starts breaking down carbohydrates. 
+1. The food is swallowed down the esophagus. It pressures down the food using coordinated muscle contration called peristalsis
+1. The food ends up in the stomach, where its mixed with gastric acid that contains hydrochloric acid(HCL) and enzymes like pepsin which helps to break down proteins. 
+1. The broken down food enters the small intenstine. Bile from the gallblader is released, as well as pancreatic juice which breaks down rest of carbohydrates, fats and proteins.  There are recursive infolding along the surface of the small intenstine called villi. Fats are absorbed into the lymphatic system while the other nutrients are absorbed into the bloodstream.
+1. What remains is sent into the large instestine, where water and ions are picked up, as well as some gut bacteria to digest what could not be broken down.
+1. The feces enters the rectum where it is later excreted via the anus.
+
+### Nutrients uptake
+
+##### Amino acids
+Proteins are built up of chains of amino acids called peptides. These amino acids are held together by peptide bonds, which are formed with a condensation reaction. They can be reveresed by hydrolysis(adding water). This happens firstly in the stomach. An enzyme pepsinogen, is secreted into the stomach. The acidic environment activates the enzyme and converts into pepsin. This makes sure that active form of the enzyme is contained within the stomach, so it does not break down the bodys own proteins. It helps facilitate the hydrolysis reaction of this proteins, mainly by targeting aromatic amino acids such as phenylalanin, tryptophan and tyrosin as substrates. Another enzyme called trypsinogen is also released and converted into trypsin, where it activates other enzymes and breaks bonds with lysine and arginine. Different peptidases then make the final breakdown of the di and tri peptides into single amino acids, which is then taken up by the blood.
+
+<br />
+
+##### Monosaccharides
+Carbohydrates, most importantly starch and amylose, is formed by chains of sugars bonded with glycosidic bonds. This can also be broken down using hydrolysis, which the enzyme amylase takes care off. It splits the carbohydrates into disaccharides. There are then multiple enzymes to break down specific disaccharides along the villi of the small intenstine, like maltase(breaks down maltose), lactase and sucrase. Active transport proteins get the carbohydrate over to the blood.
+
+<br />
+
+##### Fatty acids and other
+Lipids, mainly triglycerides and phosphilipds but also non water soluble vitamins also need to be absorbed. As they are non polar they form fat droplets, isolated from the rest of the digested food, bile salts emulsify(mix) the fat droplets into the rest of the food and split them into smaller droplets. Colipase binds to the smaller fat droplets so pancreatic lipase can bind, break down the fat droplets into free fatty acids and monoglycerides. The bile salts then form a micelle which transports the remains to the surface of the small intestine where enterocytes are. Here the remains are absorbed and later on passed onto a chylomicron, which is a structure resemblent to the micelle but with cholesterol and other proteins injected along the surface. The chylomicron is passed through the lymphatic system and into the blood stream.
+
+<br />
+
+#### Organs active in catabolism 
+
+##### Mouth
+The mouth is the first step of food digestion. It contains amylase, which breaks down carbohydrates, lysozymes, which kill bactera, antibodies, hydrogen carbonate which acts as a buffer for pH, and mucine which helps to swallow the food. Most of these substances are secreted by the salivary glands.
+##### Stomach
+The stomach can fit two literes of food. The stomach acid is very dangerous to the rest of the body and needs to be contained, mucus over the surface of the stomach protects it.
+##### Liver
+The liver is the biggest gland in the body(1.5kg). The bloodstream containg the nutrients that have been absorbed often pass through the liver for detoxification. It also produces bile, which is stored gall bladder which is important in the breakdown of lipids. It also stores many vitamins and glycogen.
+##### Pancreas
+The pancreas is under the liver and produces hormones glucagon(in alpha cells) and insulin(in beta cells), which are important in the regulation of glucose in the blood(blood sugar). Pancreatic juice is produced here, which contains amylase, lipase, trypsin, dipeptidases.
+##### Intestines
+The intestines is split up into two parts, the small and big intestine. The small intestine is about six meters long. Its surface is deeply infolded in villi and microvilli, which has enzymes that absorb the digested food. THe big instetine is shorter at 1.5meters, where water is mainly taken up. Bacteria E.coli lives here as well.
+
+### Short on blood sugar
+Blood sugar is used to refer to the amount of glucose in the blood stream. When food is digested this concentration obviousely increases, this leads to the pancreas producing more insulin which tells the liver and muslces to absorb the glucose and store it as glycogen. If the blood sugar were to get long after not eating for a long time, the pancreas will produce glucagon which triger glycogenloysis which converts glycogen to glucose. Diabetes type 1 is a disease which makes pancreas unable to produce insulin, and diabetes type 2 is a disease which makes the liver and muscles unwilling to absorb glucose even tough insulin is telling them to. Both can lead to higher levels of blood sugar.
+
+
+`}
+      </MarkdownViewer>
+    </>
+  )
+}
+ 
